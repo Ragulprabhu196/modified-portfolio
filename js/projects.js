1912a955bd42de77b42d4deacd4bef6fbbc2ba77
Ragulprabhu196 <123489882+Ragulprabhu196@users.noreply.github.com>
2023-09-21 20:59:25 +0530

    Add files via upload

---
+++ js/projects.js
@@ -0,0 +1,65 @@
+const projects = [{
+        img: "./assets/img/cloud_40534.png",
+        name: "MY DRIVE",
+        category: "fullstack",
+        techstack: ['html5', 'css-3', 'mongo', 'react-2', 'nodejs'],
+        frontend: "https://github.com/Ragulprabhu196/GoogleDrive-Clone-FRONTEND",
+        backend: "https://github.com/Ragulprabhu196/GoogleDrive-Clone-BACKEND",
+        deploymentUrl: "https://distracted-curie-61fa72.netlify.app/",
+        description: `Users can store and access files online using the cloud-based storage service MY Drive. The service synchronizes saved files, including images, across all of the user's devices, including smartphones, tablets, and PCs.`
+    },
+    {
+        img: "./assets/img/imdb.png",
+        name: " MY IMDb",
+        category: "fullstack",
+        techstack: ['html5', 'css-3', 'mongo', 'react-2', 'nodejs', 'firebase'],
+        frontend: "https://github.com/Ragulprabhu196/imdb",
+        backend: "https://github.com/Ragulprabhu196/imdb-backend",
+        deploymentUrl: "https://imdb-clone-gilt-iota.vercel.app/",
+        description: `IMDb  is an online database of information related to films, television series, podcasts, home videos, video games, and streaming content online – including cast, production crew and personal biographies, plot summaries, trivia, ratings, and fan and critical reviews.Web site that provides information about millions of films and television programs as well as their cast and crew.`
+    },
+    {
+        img: "./assets/img/gmail.jpg",
+        name: "MY GMAIL",
+        category: "fullstack",
+        techstack: ['html5', 'css-3', 'mongo', 'react-2', 'nodejs'],
+        frontend: "https://github.com/Ragulprabhu196/gmail-clone",
+        backend: "https://github.com/Ragulprabhu196/gmail-clone",
+        deploymentUrl: "playful-torrone-cf80a3.netlify.app/",
+        description: ` Gmail is an email service . This is characterized by being totally free and multi-device,so that we can access it from any device as long as we have an Internet connection.`
+    },
+    {
+        img: "./assets/img/redBus.png",
+        name: "redBus CLONE",
+        category: "fullstack",
+        techstack: ['html5', 'css-3', 'bootstrap-4', 'mongo', 'nodejs'],
+        frontend: "https://github.com/Ragulprabhu196/redbus-clone-frontend",
+        backend: "https://github.com/Ragulprabhu196/redBus-clone-backend",
+        deploymentUrl: "https://redbus.netlify.app/",
+        description: ` A redBus is India's largest online bus ticketing platform that has transformed bus travel in the country by bringing ease and convenience to millions of Indians who travel using buses.backend with the help of Node.JS and Express.Js`
+    },
+    
+    // {
+    //     img: "./assets/img/mentors.jpg",
+    //     name: "HALL BOOKING SYSTEM",
+    //     category: "backend",
+    //     techstack: ['html5', 'css-3', 'mongo', 'nodejs'],
+    //     backend: "https://github.com/Ragulprabhu196/hall-booking",
+    //     deploymentUrl: "https://documenter.getpostman.com/view/24802397/2s93Y6teg8",
+    //     description: `Hall Booking System improves the regulation and controls the functioning of applications such as booking the hall, resource needed, Time and date Duration etc..`
+    // },
+    
+    
+    {
+        img: "./assets/img/dog.jpg",
+        name: "DOG API",
+        category: "frontend",
+        techstack: ['html5', 'css-3', 'javascript-1'],
+        frontend: "https://github.com/Ragulprabhu196/Dog-Api",
+        deploymentUrl: "https://dogapi196.netlify.app/",
+        description: `With this Application we can see the picture of every Dog Breed `
+    }
+
+]
+
+//module.exports.projects = projects;
